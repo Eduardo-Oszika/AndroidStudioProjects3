3272b0586ed9c70f3712195d734893bca3132124
Eduardo-Ozika <eduardoozika.ti@gmail.com>
2025-09-09 23:13:48 -0300

    Adicionando um botão para a primeira tela e logs para cada estado da activity main

--- 2025-2/PDM1/CicloDeVida/app/src/main/java/com/oszika/ciclodevida/MainActivity.java
+++ 2025-2/PDM1/CicloDeVida/app/src/main/java/com/oszika/ciclodevida/MainActivity.java
@@ -1,24 +1,64 @@
 package com.oszika.ciclodevida;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
 
 public class MainActivity extends AppCompatActivity {
+    private Button buttonHello;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        Log.d("MAIN", "onCreate");
+        buttonHello = findViewById(R.id.buttonHello);
+        buttonHello.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent it = new Intent(MainActivity.this, SegundaActivity.class);
+                startActivity(it);
+            }
         });
+    }//onCreate
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("MAIN", "onStart");
     }
-}
+
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("MAIN", "onStop");
+    }
+
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("MAIN", "onDestroy");
+    }
+
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d("MAIN", "onPause");
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("MAIN", "onResume");
+    }
+}//class
