509c75601456575f3e80eef585d660fcae88ba10
Eduardo-Ozika <eduardoozika.ti@gmail.com>
2025-09-25 23:57:19 -0300

    Adicionando metodo para verificar o valor total e abrir os respectivas Activities CafeActivity e BoloActivity

--- 2025-2/PDM2/ExemploLanchonete/app/src/main/java/com/oszika/exemplolanchonete/MainActivity.java
+++ 2025-2/PDM2/ExemploLanchonete/app/src/main/java/com/oszika/exemplolanchonete/MainActivity.java
@@ -1,24 +1,51 @@
 package com.oszika.exemplolanchonete;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
+    private ImageView imgCafe, imgBolo;
+    private TextView textViewPreco;
+    private Calculadora calculadora;
+    private Intent intent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        intent = getIntent();
+
+
+        imgCafe = findViewById(R.id.ivCafe);
+        imgBolo = findViewById(R.id.ivBolo);
+        textViewPreco = findViewById(R.id.tvPreco);
+        calculadora = new Calculadora();
+
+        if (intent.getDoubleExtra("total", 0) != 0) {
+            double total = intent.getDoubleExtra("total", 0);
+            textViewPreco.setText("Total: R$ " + String.format("%.2f", total));
+        }
+
+        imgCafe.setOnClickListener(v -> {
+//            Snackbar.make(v, "Café selecionado", Snackbar.LENGTH_SHORT).show();
+            intent = new Intent(MainActivity.this, CafeActivity.class);
+            intent.putExtra("total", getIntent().getDoubleExtra("total", 0));
+            startActivity(intent);
         });
+        imgBolo.setOnClickListener(v -> {
+//            Snackbar.make(v, "Bolo selecionado", Snackbar.LENGTH_SHORT).show();
+            intent = new Intent(MainActivity.this, BoloActivity.class);
+            intent.putExtra("total", getIntent().getDoubleExtra("total", 0));
+            startActivity(intent);
+        });
+
     }
 }
