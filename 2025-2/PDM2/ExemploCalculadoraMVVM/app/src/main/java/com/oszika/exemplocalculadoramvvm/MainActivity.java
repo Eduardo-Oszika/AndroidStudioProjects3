3ac5727b628c1441bcfe9ba9702705ad78b16ffc
Eduardo-Ozika <eduardoozika.ti@gmail.com>
2025-09-29 20:07:20 -0300

    Activity main implementando viewmodel e fazendo evento de click no button

--- 2025-2/PDM2/ExemploCalculadoraMVVM/app/src/main/java/com/oszika/exemplocalculadoramvvm/MainActivity.java
+++ 2025-2/PDM2/ExemploCalculadoraMVVM/app/src/main/java/com/oszika/exemplocalculadoramvvm/MainActivity.java
@@ -4,21 +4,41 @@ import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.oszika.exemplocalculadoramvvm.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+    private CalculadoraViewModel viewModel;
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        //setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        viewModel = new ViewModelProvider(this).get(CalculadoraViewModel.class);
+
+        viewModel.getResultado().observe(MainActivity.this, new Observer<String>() {
+            @Override
+            public void onChanged(String s) {
+                binding.textViewResultado.setText(s);
+            }
         });
+
+        binding.buttonCalculadora.setOnClickListener(view -> {
+            String s1 = binding.editTextN1.getText().toString();
+            String s2 = binding.editTextN2.getText().toString();
+            String op = binding.editTextOp.getText().toString();
+
+            double n1 = Double.parseDouble(s1);
+            double n2 = Double.parseDouble(s2);
+            viewModel.calcularVM(n1, n2, op);
+        });
+
     }
 }
