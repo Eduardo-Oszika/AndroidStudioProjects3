f3a36e4102a365d887fc498733dba69b2c3404c7
Eduardo-Ozika <eduardoozika.ti@gmail.com>
2025-09-30 20:14:03 -0300

    Criando a lista simples utilizando o array de strings no entando para mudar basta apenas mudar o array de Strings que é passado no adapter

--- 2025-2/PDM1/ListViewSimples/app/src/main/java/com/oszika/listviewsimples/MainActivity.java
+++ 2025-2/PDM1/ListViewSimples/app/src/main/java/com/oszika/listviewsimples/MainActivity.java
@@ -1,24 +1,41 @@
 package com.oszika.listviewsimples;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.snackbar.Snackbar;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity  implements AdapterView.OnItemClickListener {
+    private ListView listViewLinguagens;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        listViewLinguagens = findViewById(R.id.lvLinguagens);
+        ArrayList<String> linguagens = Repository.getLinguagens();
+
+        String[] linguagensArray = getResources().getStringArray(R.array.linguagens_prog);
+
+//        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, linguagens);
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, linguagensArray);
+        listViewLinguagens.setAdapter(adapter);
+        listViewLinguagens.setOnItemClickListener(MainActivity.this);
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int i, long l) {
+        String linguagem = parent.getItemAtPosition(i).toString();
+        Snackbar.make(view, "Linguagem selecionada: " + linguagem, Snackbar.LENGTH_SHORT).show();
     }
 }
