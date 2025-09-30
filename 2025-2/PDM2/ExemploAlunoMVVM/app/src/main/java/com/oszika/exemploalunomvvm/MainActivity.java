f78eda917b941f5a53b7b2a6c2a8314afa617357
Eduardo-Ozika <eduardoozika.ti@gmail.com>
2025-09-29 21:08:37 -0300

    MainActivity adicionando os alunos na lista

--- 2025-2/PDM2/ExemploAlunoMVVM/app/src/main/java/com/oszika/exemploalunomvvm/MainActivity.java
+++ 2025-2/PDM2/ExemploAlunoMVVM/app/src/main/java/com/oszika/exemploalunomvvm/MainActivity.java
@@ -4,21 +4,41 @@ import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.oszika.exemploalunomvvm.databinding.ActivityMainBinding;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    private AlunoViewModel alunoViewModel;
+    private AlunoAdapter adapter;
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        alunoViewModel = new ViewModelProvider(this).get(AlunoViewModel.class);
+        adapter = new AlunoAdapter(this, new ArrayList<Aluno>());
+        binding.listViewAlunos.setAdapter(adapter);
+
+        alunoViewModel.getAlunos().observe(this, new Observer<List<Aluno>>() {
+            @Override
+            public void onChanged(List<Aluno> alunos) {
+
+                adapter.clear();
+                adapter.addAll(alunos);
+                adapter.notifyDataSetChanged();
+            }
         });
+
     }
 }
