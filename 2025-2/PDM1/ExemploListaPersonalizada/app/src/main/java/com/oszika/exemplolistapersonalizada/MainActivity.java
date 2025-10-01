ec5d72661c53bfe2ef6db10c52c39daf20e08930
Eduardo-Ozika <eduardoozika.ti@gmail.com>
2025-09-30 21:40:24 -0300

    Adicionando funcionalidade de clique na lista para exibir o nome do item

--- 2025-2/PDM1/ExemploListaPersonalizada/app/src/main/java/com/oszika/exemplolistapersonalizada/MainActivity.java
+++ 2025-2/PDM1/ExemploListaPersonalizada/app/src/main/java/com/oszika/exemplolistapersonalizada/MainActivity.java
@@ -1,6 +1,10 @@
 package com.oszika.exemplolistapersonalizada;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Adapter;
+import android.widget.AdapterView;
+import android.widget.ListView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +12,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.snackbar.Snackbar;
+
+public class MainActivity extends AppCompatActivity implements AdapterView
+
+        .OnItemClickListener{
+    ListView listView;
+    MeuAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,5 +26,16 @@ public class MainActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
+        listView = findViewById(R.id.lvItens);
+        adapter = new MeuAdapter(this, RepositorioItem.getItens());
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(this);
+
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+        ItemLista item = (ItemLista) adapterView.getItemAtPosition(i);
+        Snackbar.make(view, item.getNome(), Snackbar.LENGTH_LONG).show();
     }
 }
