2de41f3436e4b0668900df758bbf3c368a080259
Eduardo-Ozika <eduardoozika.ti@gmail.com>
2025-10-03 23:01:11 -0300

    adicionando RadioGroup para seleção de cálculo e ajustando a lógica de navegação

--- 2025-2/PDM1/BemCalcula/app/src/main/java/com/oszika/bemcalcula/ui/activity/MainActivity.java
+++ 2025-2/PDM1/BemCalcula/app/src/main/java/com/oszika/bemcalcula/ui/activity/MainActivity.java
@@ -3,6 +3,7 @@ package com.oszika.bemcalcula.ui.activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.RadioButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,9 +11,11 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.material.snackbar.Snackbar;
 import com.oszika.bemcalcula.R;
 
 public class MainActivity extends AppCompatActivity {
+    RadioButton rbBasico, rbIMC;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,15 +23,27 @@ public class MainActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
+        rbBasico = findViewById(R.id.rbCalculo);
+        rbIMC = findViewById(R.id.rbIMC);
+    }
+    public void abrirOutraTela(View view) {
+        if (rbBasico.isChecked()) {
+            calcularBasico();
+        } else if (rbIMC.isChecked()) {
+            calcularIMC();
+        }else
+            Snackbar.make(view, "Selecione uma opção", Snackbar.LENGTH_SHORT).show();
     }
 
-    public void calcularBasico(View view) {
+    public void calcularBasico() {
         Intent intent = new Intent(MainActivity.this, CalculoBasicoActivity.class);
         startActivity(intent);
     }
 
-    public void calcularIMC(View view) {
+    public void calcularIMC() {
         Intent intent = new Intent(MainActivity.this, CalculoIMCActivity.class);
         startActivity(intent);
     }
+
+
 }
