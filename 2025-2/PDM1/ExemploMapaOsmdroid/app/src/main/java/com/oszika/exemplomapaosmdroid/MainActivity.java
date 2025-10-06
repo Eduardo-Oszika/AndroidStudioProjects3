e1141a82a62d59a6129465498d94aaf8239189bc
Eduardo-Ozika <eduardoozika.ti@gmail.com>
2025-10-06 20:02:37 -0300

    Adicionando funcionalidade de mapa com marcador e exibição de coordenadas

--- 2025-2/PDM1/ExemploMapaOsmdroid/app/src/main/java/com/oszika/exemplomapaosmdroid/MainActivity.java
+++ 2025-2/PDM1/ExemploMapaOsmdroid/app/src/main/java/com/oszika/exemplomapaosmdroid/MainActivity.java
@@ -1,24 +1,71 @@
 package com.oszika.exemplomapaosmdroid;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import org.osmdroid.config.Configuration;
+import org.osmdroid.util.GeoPoint;
+import org.osmdroid.views.MapView;
+import org.osmdroid.views.overlay.Marker;
+
 public class MainActivity extends AppCompatActivity {
+    private MapView map;
+    private TextView textViewCoordenadas;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        Configuration.getInstance().setUserAgentValue(getPackageName());
+        map = findViewById(R.id.mapa);
+        textViewCoordenadas = findViewById(R.id.coordenadas);
+        inicializarMapa();
+
+    }
+//    -10.1987632,-48.3129125
+
+    private void inicializarMapa() {
+        if (map != null) {
+            map.setMultiTouchControls(true);
+            GeoPoint palmas = new GeoPoint(-10.1987632, -48.3129125);
+
+            map.getController().setZoom(15.0);
+            map.getController().setCenter(palmas); // Coordenadas de Madrid
+
+            Marker marker = new Marker(map);
+            marker.setPosition(palmas);
+            marker.setTitle("Palmas IFTO");
+            marker.setIcon(ContextCompat.getDrawable(this, R.drawable.sol));
+            map.getOverlays().add(marker);
+            marker.setDraggable(true);
+
+            marker.setOnMarkerDragListener(new Marker.OnMarkerDragListener() {
+                @Override
+                public void onMarkerDragStart(Marker marker) {
+                    // Ação ao iniciar o arrasto (opcional)
+                }
+
+                @Override
+                public void onMarkerDrag(Marker marker) {
+                    // Ação durante o arrasto (opcional)
+                }
+
+                @Override
+                public void onMarkerDragEnd(Marker marker) {
+                    GeoPoint position = marker.getPosition();
+                    String coordenadas = "Lat: " + position.getLatitude() + "\nLong: " + position.getLongitude();
+                    textViewCoordenadas.setText(coordenadas);
+                }
+            });
+        }
     }
 }
