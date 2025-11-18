644dea4860e81c4e70284ec591fe1d124573abc8
Eduardo-Ozika <eduardoozika.ti@gmail.com>
2025-11-18 00:16:04 -0300

    Implementa a funcionalidade de login com SharedPreferences

--- 2025-2/PDM1/SharedPreferencesQuiz/app/src/main/java/com/oszika/sharedpreferencesquiz/MainActivity.java
+++ 2025-2/PDM1/SharedPreferencesQuiz/app/src/main/java/com/oszika/sharedpreferencesquiz/MainActivity.java
@@ -1,24 +1,49 @@
 package com.oszika.sharedpreferencesquiz;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText etName, etSenha;
+    Button btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        etName = findViewById(R.id.et_usuario);
+        etSenha = findViewById(R.id.et_senha);
+        btnLogin = findViewById(R.id.btnLogin);
+
+        btnLogin.setOnClickListener(v -> {
+            String name = etName.getText().toString();
+            String senha = etSenha.getText().toString();
+
+            if (name.equals("admin") && senha.equals("1234")) {
+
+                SharedPreferences prefs = getSharedPreferences("APP_PREFS", MODE_PRIVATE);
+                SharedPreferences.Editor editor = prefs.edit();
+                editor.putBoolean("logado", true);
+                editor.apply();
+
+                Intent i = new Intent(MainActivity.this, SegundaActivity.class);
+                startActivity(i);
+                finish();
+
+            } else {
+                Toast.makeText(this, "Usuário ou senha incorretos!", Toast.LENGTH_SHORT).show();
+            }
         });
     }
 }
