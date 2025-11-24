0cafffd8e6fdbe9f9e9df8de8284497c4571d98a
Eduardo-Ozika <eduardoozika.ti@gmail.com>
2025-11-24 20:24:47 -0300

    Exemplo Simples de Handler para uma splasScreen

--- 2025-2/PDM2/ExemploHandler/app/src/main/java/com/oszika/exemplohandler/MainActivity.java
+++ 2025-2/PDM2/ExemploHandler/app/src/main/java/com/oszika/exemplohandler/MainActivity.java
@@ -1,6 +1,10 @@
 package com.oszika.exemplohandler;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +13,22 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    private static int TIME = 3000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                chamarActivity();
+            }
+        },TIME);
+    }
+    private void chamarActivity(){
+        Intent intent = new Intent(MainActivity.this, SegundaActivity.class);
+        startActivity(intent);
     }
 }
