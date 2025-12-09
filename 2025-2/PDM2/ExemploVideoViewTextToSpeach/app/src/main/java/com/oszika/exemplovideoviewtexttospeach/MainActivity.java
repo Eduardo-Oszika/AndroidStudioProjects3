d8e1ac555b749a9e2202295d5c36f0d903d3c790
Eduardo-Ozika <eduardoozika.ti@gmail.com>
2025-12-09 19:46:18 -0300

    Implemente layouts de atividade principal e secundária com recursos de reprodução de vídeo e entrada de texto.

--- 2025-2/PDM2/ExemploVideoViewTextToSpeach/app/src/main/java/com/oszika/exemplovideoviewtexttospeach/MainActivity.java
+++ 2025-2/PDM2/ExemploVideoViewTextToSpeach/app/src/main/java/com/oszika/exemplovideoviewtexttospeach/MainActivity.java
@@ -1,6 +1,10 @@
 package com.oszika.exemplovideoviewtexttospeach;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +13,25 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    private Button button;
+    private EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        button = findViewById(R.id.buttonProximo);
+        editText = findViewById(R.id.editTextNome);
+
     }
+
+    public void clicar(View view) {
+        String nome = editText.getText().toString();
+        Intent intent = new Intent(this, SegundaActivity.class);
+        intent.putExtra("nome", nome);
+        startActivity(intent);
+    }
+
 }
